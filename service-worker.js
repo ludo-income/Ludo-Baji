1843f77b8d45a2642e0756047ca40c8d1145d3ff
ludo-income <rakibahmedrana270@gmail.com>
2026-09-13 23:09:12 +0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* Ludo Baji Service Worker - PWA Install + Push */
-const CACHE_NAME = 'ludo-baji-v3';
+const CACHE_NAME = 'ludo-baji-v4';
 const PRECACHE = ['/', '/index.html', '/admin', '/admin.html', '/manifest.webmanifest', '/admin-manifest.webmanifest', '/7543.jpg', '/logo-ludo-baji.jpg'];
 
 self.addEventListener('install', (event) => {
